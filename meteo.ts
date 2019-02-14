31fb94a5f39e543107e97559992ef379342c5c0e
Lorick <lorick@live.co.uk>
2019-02-14 20:39:15 +0100

    oubli

---
+++ meteo.ts
@@ -0,0 +1,10 @@
+class Meteo {
+    constructor(private _ville: string, private _temperature: number) {
+
+    }
+    toString(): string {
+        return `${this._ville} - ${this._temperature}°C`
+    }
+}
+let nantesCeMatin = new Meteo('Nantes', 12);
+console.log(nantesCeMatin.toString())
